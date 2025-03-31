5738874813eb985191921e1c14d266321365f40f
gdemartino93 <g.demartino9393@gmail.com>
2025-04-01 00:07:35 +0200

    created 1st section

---
+++ MVVM/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUI_ToDo.MVVM.Models
+{
+    [AddINotifyPropertyChangedInterface]
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int PendingTasks { get; set; }
+        public float Percentage { get; set; }
+    }
+}
--- MVVM/ViewModels/MainViewModel.cs
+++ MVVM/ViewModels/MainViewModel.cs
@@ -1,12 +1,45 @@
-﻿using System;
+﻿using MAUI_ToDo.MVVM.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MAUI_ToDo.MVVM.ViewModels
 {
-    class MainViewModel
+    public class MainViewModel
     {
+        public ObservableCollection<Category> Categories { get; set; }
+        public MainViewModel()
+        {
+            FillData();
+        }
+
+        private void FillData()
+        {
+            Categories = new ObservableCollection<Category>
+            {
+                new Category
+                {
+                    Id = 1,
+                    Name = ".NET Maui",
+                    Color = "#f44336"
+
+                },
+                new Category
+                {
+                    Id = 2,
+                    Name = "Certificazioni Azure",
+                    Color = "#e69138"
+                },
+                new Category
+                {
+                    Id = 3,
+                    Name = "Studio altro",
+                    Color = "#8e7cc3"
+                }
+            };
+        }
     }
 }
