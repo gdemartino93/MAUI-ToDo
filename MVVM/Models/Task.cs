f8b52ef6229fc8157e38b3a2e4d2465db7aeb64d
gdemartino93 <g.demartino9393@gmail.com>
2025-04-01 00:33:48 +0200

    created categories section

---
+++ MVVM/Models/Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUI_ToDo.MVVM.Models
+{
+    public class Task
+    {
+        public string Name { get; set; }
+        public bool IsCompleted { get; set; }
+        public string? Color { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
--- MVVM/ViewModels/MainViewModel.cs
+++ MVVM/ViewModels/MainViewModel.cs
@@ -5,15 +5,18 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Task = MAUI_ToDo.MVVM.Models.Task;
 
 namespace MAUI_ToDo.MVVM.ViewModels
 {
     public class MainViewModel
     {
         public ObservableCollection<Category> Categories { get; set; }
+        public ObservableCollection<Task> Tasks { get; set; }
         public MainViewModel()
         {
             FillData();
+            UpdateData();
         }
 
         private void FillData()
@@ -40,6 +43,52 @@ namespace MAUI_ToDo.MVVM.ViewModels
                     Color = "#8e7cc3"
                 }
             };
+            Tasks = new ObservableCollection<Models.Task>
+            {
+                new Task
+                {
+                    Name = "AZ-900",
+                    IsCompleted = true,
+                    CategoryId = 2
+                },
+                new Task
+                {
+                    Name = "AZ-204",
+                    IsCompleted = false,
+                    CategoryId = 2
+                },
+                new Task
+                {
+                    Name = "Creare app per X",
+                    IsCompleted = false,
+                    CategoryId = 1
+                },
+                new Task
+                {
+                    Name = "AI",
+                    IsCompleted = false,
+                    CategoryId = 3
+                }
+            };
+        }
+        private void UpdateData()
+        {
+            foreach (var category in Categories)
+            {
+                IEnumerable<Task> tasks = Tasks.Where(t => t.CategoryId == category.Id);
+                IEnumerable<Task> tasksCompleted = tasks.Where(t => t.IsCompleted);
+                IEnumerable<Task> tasksNotCompleted = tasks.Where(t => !t.IsCompleted);
+
+                category.PendingTasks = tasksNotCompleted.Count();
+                category.Percentage = (float)tasksCompleted.Count() / (float)tasks.Count();
+
+                foreach (var task in Tasks)
+                {
+                    string? categoryColor = Categories.Where(c => c.Id == task.CategoryId).Select(c => c.Name).FirstOrDefault();
+                    task.Color = categoryColor;
+                }
+
+            }
         }
     }
 }
