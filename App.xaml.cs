6bbcf9aa9807fa210c5ea30d1115dccc97a488be
gdemartino93 <g.demartino9393@gmail.com>
2025-04-01 23:53:58 +0200

    removed underlined border from entry control

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using MAUI_ToDo.MVVM.Views;
+using Microsoft.Maui.Platform;
 
 namespace MAUI_ToDo
 {
@@ -8,7 +9,29 @@ namespace MAUI_ToDo
         {
             InitializeComponent();
 
-            MainPage = new MainView();
+            MainPage = new NewTaskView();
+
+#if ANDROID
+            Microsoft.Maui.Handlers.EntryHandler.Mapper
+                .AppendToMapping(nameof(Entry), (handler, view) =>
+                {
+                    handler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Colors.Transparent.ToPlatform());
+                });
+#endif
+
+#if IOS
+
+            Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(nameof(Entry), (handler, view) =>
+            {
+                handler.PlatformView.BorderStyle = UITextBorderStyle.None;
+
+                handler.PlatformView.BackgroundColor = UIColor.Clear;
+
+                handler.PlatformView.LeftView = new UIView(new CoreGraphics.CGRect(0, 0, 8, 0));
+                handler.PlatformView.LeftViewMode = UITextFieldViewMode.Always;
+            });
+#endif
+
         }
     }
 }
